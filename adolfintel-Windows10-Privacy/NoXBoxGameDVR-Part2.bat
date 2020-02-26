a967a8662e5e6188e07d1698286d74d2014a7370
L33Tech <36166244+L33Tech@users.noreply.github.com>
2020-02-26 11:53:47 -0500

    Create NoXBoxGameDVR-Part2.bat

---
+++ adolfintel-Windows10-Privacy/NoXBoxGameDVR-Part2.bat
@@ -0,0 +1,9 @@
+sc delete XblAuthManager
+sc delete XblGameSave
+sc delete XboxNetApiSvc
+sc delete XboxGipSvc
+reg delete "HKLM\SYSTEM\CurrentControlSet\Services\xbgm" /f
+schtasks /Change /TN "Microsoft\XblGameSave\XblGameSaveTask" /disable
+schtasks /Change /TN "Microsoft\XblGameSave\XblGameSaveTaskLogon" /disable
+reg add "HKLM\SOFTWARE\Policies\Microsoft\Windows\GameDVR" /v AllowGameDVR /t REG_DWORD /d 0 /f
+pause
