459c7b0933cb855cf3d6b159d709b9931cf89416
L33Tech <L33Tech@users.noreply.github.com>
2020-04-08 11:24:07 -0400

    Add Windows Repair script

---
+++ Scripts/RepairWindows.bat
@@ -0,0 +1,39 @@
+@echo off
+title Windows10Tools - Windows Repair Utility
+REM This scrip is a part of Windows10Tools. Find it here: https://github.com/L33Tech/Windows10Tools
+:: BatchGotAdmin
+REM BatchGotAdmin code sourced from https://stackoverflow.com/questions/1894967/how-to-request-administrator-access-inside-a-batch-file
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params= %*
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params:"=""%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------    
+REM Now, to actually run the repair operation...
+echo Cleaning up Windows Image and restoring health....
+dism /online /cleanup-image /restorehealth
+echo Scanning for Windows Integrity violations....
+sfc /scannow
+echo Scan Complete!
+pause
