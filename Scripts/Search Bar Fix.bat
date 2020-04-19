3430c26cd62776454e5ac257a822eb2c92a15adf
L33Tech <36166244+L33Tech@users.noreply.github.com>
2020-04-19 13:59:04 -0400

    Registry Patch Scripts

---
+++ Scripts/Search Bar Fix.bat
@@ -0,0 +1,34 @@
+@echo off
+REM This script is a part of Windows10Tools. Find it here: https://github.com/L33Tech/Windows10Tools
+REM Code snippets from unknown source.
+REM This file was originally a .reg
+title Windows10Tools - Search Bar Fix
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "AllowCortana" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "BingSearchEnabled" /t REG_DWORD /d "0" /f
+Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Windows Search" /v "CortanaConsent" /t REG_DWORD /d "0" /f
+echo Complete!
+pause
---
+++ Scripts/Some Settings Are Managed Fix.bat
@@ -0,0 +1,79 @@
+@echo off
+REM This script is a part of Windows10Tools. Find it here: https://github.com/L33Tech/Windows10Tools
+REM This file was originally a .reg
+title Windows10Tools - Some Settings are Managed Fix
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+REM Original .reg from https://appuals.com/how-to-fix-some-settings-are-managed-by-your-organization/ converted to BAT by L33Tech for Windows10Tools
+Reg.exe delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" /v "NoChangingWallpaper" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" /f
+Reg.exe delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "NoDispAppearancePage" /f
+Reg.exe delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "NoDispScrSavPage" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f
+Reg.exe delete "HKCU\SOFTWARE\Policies\Microsoft\Windows\Personalization" /v "NoChangingLockScreen" /f
+Reg.exe delete "HKCU\SOFTWARE\Policies\Microsoft\Windows\Personalization" /v "NoChangingSoundScheme" /f
+Reg.exe delete "HKCU\SOFTWARE\Policies\Microsoft\Windows\Personalization" /v "NoChangingStartMenuBackground" /f
+Reg.exe delete "HKCU\SOFTWARE\Policies\Microsoft\Windows\Personalization" /v "NoLockScreen" /f
+Reg.exe add "HKCU\SOFTWARE\Policies\Microsoft\Windows\Personalization" /f
+Reg.exe delete "HKCU\SOFTWARE\Policies\Microsoft\Windows\System" /v "DisableLockScreenAppNotifications" /f
+Reg.exe delete "HKCU\SOFTWARE\Policies\Microsoft\Windows\System" /v "DisableLogonBackgroundImage" /f
+Reg.exe add "HKCU\SOFTWARE\Policies\Microsoft\Windows\System" /f
+Reg.exe delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoChangeStartMenu" /f
+Reg.exe delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoControlPanel" /f
+Reg.exe delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoSetTaskbar" /f
+Reg.exe delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoStartMenuMFUprogramsList" /f
+Reg.exe delete "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoThemesTab" /f
+Reg.exe add "HKCU\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /f
+Reg.exe delete "HKCU\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "ForceStartSize" /f
+Reg.exe delete "HKCU\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "LockedStartLayout" /f
+Reg.exe add "HKCU\SOFTWARE\Policies\Microsoft\Windows\Explorer" /f
+Reg.exe delete "HKCU\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v "DisableWindowsConsumerFeatures" /f
+Reg.exe add "HKCU\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /f
+Reg.exe delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" /v "NoChangingWallpaper" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\ActiveDesktop" /f
+Reg.exe delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "NoDispAppearancePage" /f
+Reg.exe delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /v "NoDispScrSavPage" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" /f
+Reg.exe delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\Personalization" /v "NoChangingLockScreen" /f
+Reg.exe delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\Personalization" /v "NoChangingSoundScheme" /f
+Reg.exe delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\Personalization" /v "NoChangingStartMenuBackground" /f
+Reg.exe delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\Personalization" /v "NoLockScreen" /f
+Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Personalization" /f
+Reg.exe delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "DisableLockScreenAppNotifications" /f
+Reg.exe delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /v "DisableLogonBackgroundImage" /f
+Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\System" /f
+Reg.exe delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoChangeStartMenu" /f
+Reg.exe delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoControlPanel" /f
+Reg.exe delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoSetTaskbar" /f
+Reg.exe delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoStartMenuMFUprogramsList" /f
+Reg.exe delete "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /v "NoThemesTab" /f
+Reg.exe add "HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer" /f
+Reg.exe delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "ForceStartSize" /f
+Reg.exe delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /v "LockedStartLayout" /f
+Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\Explorer" /f
+Reg.exe delete "HKLM\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /v "DisableWindowsConsumerFeatures" /f
+Reg.exe add "HKLM\SOFTWARE\Policies\Microsoft\Windows\CloudContent" /f
+echo Complete!
+pause
