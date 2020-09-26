65b4f8771ddb37517f57966ac88616b9bf9888b4
L33Tech <36166244+L33Tech@users.noreply.github.com>
2020-09-26 14:31:32 -0400

    add firewall managemnent

---
+++ Scripts/Disable Firewall.bat
@@ -0,0 +1,32 @@
+@echo off
+REM This script is a part of Windows10Tools. Find it here: https://github.com/L33Tech/Windows10Tools
+title Windows10Tools - Disable Firewall
+REM BatchGotAdmin code sourced from https://stackoverflow.com/questions/1894967/how-to-request-administrator-access-inside-a-batch-file
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+@echo on
+NetSh Advfirewall set allprofiles state off
+@echo off
+pause
